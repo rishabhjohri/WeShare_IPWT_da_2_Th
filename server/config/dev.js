b55044daeb6d6f8c195b7d87ca190b2a11a9f5e5
rishabhjohri <54755134+rishabhjohri@users.noreply.github.com>
2021-05-26 13:45:32 +0530

    Update dev.js

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI:'mongodb+srv://rish:HTkxY7FiJ7snppf@cluster0.8cc88.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    mongoURI:'mongodb+srv://rish:HTkxY7FiJ7snppf@cluster0.8cc88.mongodb.net/test?retryWrites=true&w=majority'
 }
